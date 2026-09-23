0a8a1d1944f3b898d534ffd6eaf596f1ea29a560
Naqeeb669 <naqeeblat@gmail.com>
2026-09-24 03:47:02 +0800

    Fix all

--- config/db.php
+++ config/db.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $host = getenv('DB_HOST');
@@ -9,14 +8,7 @@ $password = getenv('DB_PASSWORD');
 
 $conn = mysqli_init();
 
-mysqli_ssl_set(
-    $conn,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-);
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
 if (!mysqli_real_connect(
     $conn,
@@ -28,11 +20,5 @@ if (!mysqli_real_connect(
     NULL,
     MYSQLI_CLIENT_SSL
 )) {
-    die(
-        "Database connection failed: " .
-        mysqli_connect_error()
-    );
+    die("Database connection failed: " . mysqli_connect_error());
 }
-
-mysqli_set_charset($conn, "utf8mb4");
-
