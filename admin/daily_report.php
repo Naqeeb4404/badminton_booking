1cb86fc6f697bfb868a521b9361db879908f761d
Naqeeb669 <naqeeblat@gmail.com>
2026-09-26 14:16:59 +0800

    Fix all

--- admin/daily_report.php
+++ admin/daily_report.php
@@ -119,8 +119,8 @@ $courtUsageResult = $courtUsageQuery->get_result();
                 </div>
             </div>
         </div>
+    </main>
     </div>
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html></main></div></body></html>
+</html>
